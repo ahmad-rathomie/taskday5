25b4c73aae4688f8194a3ba42b20eb21dafec40b
mohdamirfaris <149853681+mohdamirfaris@users.noreply.github.com>
2023-11-18 17:04:46 +0800

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,25 @@
+// Write a JavaScript script that declares two variables, `num1` and `num2`, and assigns them numeric values.
+
+let num1 = 1;
+let num2 = 2;
+
+// Perform basic arithmetic operations (addition, subtraction, multiplication, and division) on these variables.
+
+//addition
+let sum = num1 + num2;
+
+//substraction
+let diff = num1 - num2;
+
+//multiplication
+let mult = num1 * num2;
+
+//division
+let div = num1 / num2;
+
+// Print the results to the console with meaningful messages.
+
+console.log("The sum of " + num1 + " and " + num2 + " is " + sum);
+console.log("The difference between " + num1 + " and " + num2 + " is " + diff);
+console.log("The product of " + num1 + " and " + num2 + " is " + mult);
+console.log("The division of " + num1 + " and " + num2 + " is " + div);
