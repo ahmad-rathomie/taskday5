560aca738d7baa5dd9b0142d1c6639c83dd5d1eb
tommy <tommy>
2023-11-18 16:32:33 +0800

    new

---
+++ index.js
@@ -0,0 +1,4 @@
+var num1 = 3
+var num2 =5
+
+document.write(num1+num2);
---
+++ tasktwo.js
@@ -0,0 +1,18 @@
+
+
+
+function checkNumber(num){
+    num = document.getElementById("check").value;
+    if (num <0){
+        console.log("negative");
+    }
+    else if(num >0){
+        console.log("positive");
+
+        }
+            
+        else {console.log("is zero");}
+    
+    
+
+}
